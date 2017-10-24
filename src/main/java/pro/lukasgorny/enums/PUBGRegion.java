5acd5583b11c279b998e043728264be0eddc344e
Łukasz <lukasz.p.gorny@gmail.com>
2017-10-24 16:08:27 +0200

    Version 1.0.5
    * Added full region names to regions
    * Added support for pre-release season 5
    * Added support for Korea/Japan region (KRJP)

--- src/main/java/pro/lukasgorny/enums/PUBGRegion.java
+++ src/main/java/pro/lukasgorny/enums/PUBGRegion.java
@@ -2,13 +2,24 @@ package pro.lukasgorny.enums;
 
 /**
  * Created by Łukasz "Husar" Górny on 2017-06-29.
- */
+*/
 public enum PUBGRegion {
-    agg,
-    na,
-    eu,
-    as,
-    oc,
-    sa,
-    sea
+    agg("All Regions Combined"),
+    na("North America"),
+    eu("Europe"),
+    as("Asia"),
+    oc("Oceania"),
+    sa("South America"),
+    sea("South East Asia"),
+    krjp("Korea/Japan");
+
+    private final String regionName;
+
+    PUBGRegion(String regionName) {
+        this.regionName = regionName;
+    }
+
+    public String getRegionName() {
+        return regionName;
+    }
 }
